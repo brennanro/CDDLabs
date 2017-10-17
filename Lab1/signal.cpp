4889b00bc9c8673a7a9f6a713b8f5c20f6a50e86
brennanro <c00180359@itcarlow.ie>
2017-10-17 14:55:55 +0100

    <Message>

--- Lab1/signal.cpp
+++ Lab1/signal.cpp
@@ -2,19 +2,29 @@
 #include <iostream>
 #include <thread>
 
+/**
+   Author: Ronan Brennan 
+   Date: 14/10/2017
+
+   Using Semaphore signal and wait to output values
+**/
 void taskOne(std::shared_ptr<Semaphore> theSemaphore){
+  std::cout<<"Task One Entered"<<std::endl; /**Check that Task one is entered**/
   std::cout <<"I ";
   std::cout << "must ";
   std::cout << "print ";
-  std::cout << "first"<<std::endl;
-  theSemaphore->Signal();
+  std::cout << "first"<<std::endl; 
+  theSemaphore->Signal();/**Send Signal**/
+  std::cout<<"Task One Finished"<<std::endl;/**Check Task one is finished**/
 }
 void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
-  theSemaphore->Wait();
+  std::cout<<"Task Two Entered"<<std::endl;
+  theSemaphore->Wait();/**Won't continue until signal is received**/
   std::cout <<"This ";
   std::cout << "will ";
   std::cout << "appear ";
   std::cout << "second"<<std::endl;
+  std::cout<<"Task Two Finished"<<std::endl;
 }
 
 int main(void){
@@ -26,5 +36,6 @@ int main(void){
   std::cout << "Launched from the main\n";
   threadOne.join();
   threadTwo.join();
+  std:: cout<<"Join Successful"<<std::endl;/**Outputs when works correctly**/
   return 0;
 }
